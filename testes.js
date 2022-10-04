ac986a76067b5eeb7bdeca2b45a29ef6db0c5eed
Juliana Araujo <julianaaraujo21@gmail.com>
2022-10-04 20:19:12 -0300

    Testes variados de tipos de variaveis JavaScript

--- testes.js
+++ testes.js
@@ -4,3 +4,10 @@ const minhavar = "3";
 const MINHAVAR = 2;
 
 console.log(minhaVar, MinhaVar, minhavar, MINHAVAR)
+
+
+const soma = 10 + 10;
+const multiplicacao = 10 * 10;
+const operacao = (soma + multiplicacao) + 10;
+
+console.log(operacao)
